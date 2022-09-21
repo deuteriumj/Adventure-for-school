61a58047e1e69eeb175608ab683dd51dd5829ac5
taranakitom <tomburgess@slingshot.co.nz>
2022-09-21 23:35:15 +1200

    colour text

--- Adventure for school/Game.py
+++ Adventure for school/Game.py
@@ -17,6 +17,7 @@ availableInteractions = []
 availableResults = []
 inventory = []
 
+# list of items usable anywhere
 anywhereInteractions = placeJson["anywhereItems"]
 
 houseOpen = False
@@ -25,6 +26,15 @@ hasWon = False
 PHONE_PASSWORD = random.randint(100000, 999999)
 PHONE_PASSWORDPAGE = str(random.randint(11, 20))
 
+RED = 31
+GREEN = 32
+YELLOW = 33
+BLUE = 34
+PURPLE = 35
+CYAN = 36
+
+def colourtext(colour, text):
+    return f'\033[{colour}m{text}\033[0m'
 
 def displayIntro():
     print()
@@ -42,13 +52,14 @@ def actionsUI():
 
 displayIntro()
 while not hasWon:
+    # set the room around the player based on which room they're in
     availableInteractions = placeJson["places"][currentRoom]["availableInteractions"]
     currentMovements = placeJson["places"][currentRoom]["connectedPlaces"]
     currentDesc = placeJson["places"][currentRoom]["description"]
     availableItems = placeJson["places"][currentRoom]["items"]
     availableResults = placeJson["places"][currentRoom]["interactionResults"]
 
-    action = input(f"\n(To check the commands type \"actions\")\nYou are in the \033[0;31;40m{currentRoom}\033[0m\n{currentDesc}\nMovement options:\n\033[0;32;40m{' '.join(currentMovements)}\033[0m\nAvailable items:\n\033[0;33;40m{' '.join(availableItems)}\033[0m\n>").upper()
+    action = input(f"\n(To check the commands type \"actions\")\nYou are in the {colourtext(GREEN, currentRoom)}\n{currentDesc}\nMovement options:\n{colourtext(BLUE, ' '.join(currentMovements))}\nAvailable items:\n{colourtext(YELLOW, ' '.join(availableItems))}\n>").upper()
     
     if action == "INVENTORY":
         inventoryUI()
@@ -56,7 +67,7 @@ while not hasWon:
         actionsUI()
 
     elif "TAKE " in action:
-        # check if the item in action is also in available items
+        # check if the item they wanted to take is available
         for i in availableItems:
             if i in action:
                 print(placeJson["places"][currentRoom]["itemDescriptions"][i])
@@ -66,27 +77,30 @@ while not hasWon:
                 input("Press enter to continue...")
 
     elif "GO TO " in action:
-        # check if the place in action is also in the available movements
+        # check if the place they wanted to go to is available
         for i in currentMovements:
             if i in action:
                 currentRoom = i
 
     elif "USE " in action:
-        # check if the item in action is also in available interactions and inventory
+        # check if the item they wanted to use is available
         for i in availableInteractions:
             if i in action and i in inventory:
                 print(availableResults[i])
+                # shovel will turn DIRT_PATCH into THE_HOLE and move you into it
                 if i == "SHOVEL":
                     placeJson["places"]["FOREST"]["connectedPlaces"].remove("DIRT_PATCH")
                     placeJson["places"]["FOREST"]["connectedPlaces"].append("THE_HOLE")
                     currentRoom = "THE_HOLE"
                 
+                # STRANGE_KEY will open the chest and give you the HOUSE_KEY
                 elif i == "STRANGE_KEY":
                     inventory.append("HOUSE_KEY")
                     print("You got HOUSE_KEY")
                     print(placeJson["places"][currentRoom]["itemDescriptions"]["HOUSE_KEY"])
                     input("Press enter to continue...")
                 
+                # HOUSE_KEY will open the house
                 elif i == "HOUSE_KEY":
                     houseOpen = True
 
